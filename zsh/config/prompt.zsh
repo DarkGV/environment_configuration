a594b97b675d18359441211ba6cbb9ff2b4ee4d2
Diogo Almirante <diogo.almirante@miniclip.com>
2021-10-21 15:35:47 +0100

    Add git mode to prompt info

--- zsh/config/prompt.zsh
+++ zsh/config/prompt.zsh
@@ -1,6 +1,7 @@
+STRICT_MODE=${STRICT_MODE:-"true"}
 OTP_VERSION=$(kerl prompt | tr -d ' ()')
 right_prompt() {
-    echo "%F{6}$REBAR_PROFILE %F{3}$AWS_PROFILE %F{4}$AWS_DEFAULT_REGION %F{1}OTP($OTP_VERSION)%{$reset_color%}"
+    echo "$STRICT_MODE%F{6}$REBAR_PROFILE %F{3}$AWS_PROFILE %F{4}$AWS_DEFAULT_REGION %F{1}OTP($OTP_VERSION)%{$reset_color%}"
 }
 
 RPROMPT+='$(right_prompt)'
